de23146409ab868a7b497a8a0f2d84d333c1dea6
micknvb <micknvb@gmail.com>
2026-04-03 17:11:39 +0200

    Change CategoryId and ParentId to long type

--- Jellyfin.Xtream/Client/Models/Category.cs
+++ Jellyfin.Xtream/Client/Models/Category.cs
@@ -1,31 +1,45 @@
-﻿// Copyright (C) 2022  Kevin Jilissen
-
+// Copyright (C) 2022 Kevin Jilissen
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 // GNU General Public License for more details.
-
 // You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 using Newtonsoft.Json;
 
 #pragma warning disable CS1591
 namespace Jellyfin.Xtream.Client.Models;
 
+/// <summary>
+/// Represents a channel/VOD/series category as returned by
+/// get_live_categories, get_vod_categories, or get_series_categories.
+///
+/// Dispatcharr (and several other Xtream-compatible servers) may return
+/// category_id and parent_id as quoted strings → FlexibleLongConverter.
+/// </summary>
 public class Category
 {
+    /// <summary>
+    /// Unique category identifier.
+    /// Dispatcharr may return this as a quoted string.
+    /// </summary>
     [JsonProperty("category_id")]
-    public int CategoryId { get; set; }
+    [JsonConverter(typeof(FlexibleLongConverter))]
+    public long CategoryId { get; set; }
 
     [JsonProperty("category_name")]
     public string CategoryName { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Parent category identifier (0 = top-level).
+    /// Dispatcharr may return this as a quoted string.
+    /// </summary>
     [JsonProperty("parent_id")]
-    public int ParentId { get; set; }
+    [JsonConverter(typeof(FlexibleLongConverter))]
+    public long ParentId { get; set; }
 }
